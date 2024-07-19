4c6db69b4efde2e23b2acdd7e0859d9db21330c9
pengh-mbp <qingseyunlan@gmail.com>
2024-07-19 12:23:02 +0800

    v0.2.0, refactor, use mod

---
+++ src/crypt.rs
@@ -0,0 +1,227 @@
+use base64::{engine::general_purpose::URL_SAFE, Engine as _};
+use hmac::{Hmac, Mac};
+use log::{debug, trace, warn};
+use regex::Regex;
+use sha2::{Digest, Sha256};
+use std::{
+    fs,
+    io::Write,
+    process::{Command, Stdio},
+};
+
+use crate::{write_file, AcmeError, Alg, CERT_BEGIN, CERT_REGEX, DOMAIN_KEY, ISSUER_REGEX, TIP_REGEX_FAILED};
+
+pub fn base64_hmac256(key: &str, s: &str) -> String {
+    type HmacSha256 = Hmac<Sha256>;
+    let mut mac = HmacSha256::new_from_slice(&URL_SAFE.decode(_durl_base64(key)).unwrap()).expect("...");
+    mac.update(s.as_bytes());
+    base64(&mac.finalize().into_bytes())
+}
+
+fn _durl_base64(input: &str) -> String {
+    let padding = match input.len() % 4 {
+        2 => "==",
+        3 => "=",
+        _ => "",
+    };
+    let mut output = input.to_string();
+    output.push_str(padding);
+    output.replace("_-", "+/")
+}
+
+pub fn base64(s: &[u8]) -> String {
+    URL_SAFE.encode(s).replace("=", "")
+}
+
+pub fn base64_hex(hex_str: &str) -> String {
+    base64(&hex::decode(hex_str).unwrap())
+}
+pub fn base64_sha256(p: &str) -> String {
+    //let p = r#"{"crv":"P-256","kty":"EC","x":"MysViqQWRtiId88Tr5-PkZzLQ64WagPZF_WFPJk_LIE","y":"WByhhlb7q50I-uXme6YSG042gMslQuiy1st36FUn3MQ"}"#;
+    let mut hasher = Sha256::new();
+    hasher.update(p.as_bytes());
+    let hash = hasher.finalize();
+    let b64_hash = base64(&hash);
+    trace!("sha2 sha256 base64: {}", b64_hash);
+    b64_hash
+}
+
+pub fn gen_key_by_cmd_openssl(alg: &Alg) -> Result<Vec<u8>, AcmeError> {
+    let a: Vec<&str> = match alg {
+        Alg::RSA2048 => vec!["genrsa", "2048"],
+        Alg::RSA4096 => vec!["genrsa", "4096"],
+        Alg::ECC256 => vec!["ecparam", "-name", "prime256v1", "-genkey"],
+        Alg::ECC384 => vec!["ecparam", "-name", "secp384r1", "-genkey"],
+    };
+
+    let out = Command::new("openssl").args(a).output()?;
+    Ok(out.stdout)
+}
+
+// echo "[SAN]\nsubjectAltName=$DNS" > openssl.cnf.1.tmp
+// cat /etc/ssl/openssl.cnf openssl.cnf.1.tmp > openssl.cnf.tmp
+// openssl req -new -key domain.key -subj "/" -reqexts SAN -config openssl.cnf.tmp  > domain.csr
+pub fn gen_csr_by_cmd_openssl(acme_dir: &str, domain_key_alg: &Alg, dns: &Vec<String>) -> Result<(String, String), AcmeError> {
+    let domain_key_path = format!("{0}/{1}", acme_dir, DOMAIN_KEY);
+    let domain_csr_path = format!("{}/domain.csr", acme_dir);
+    let tmp = format!("{}/openssl.cnf.tmp", acme_dir);
+
+    let _ = write_file(&domain_key_path, &gen_key_by_cmd_openssl(&domain_key_alg)?)?;
+    debug!("Successfully. Gen domain key by {:?}: {}", &domain_key_alg, &domain_key_path);
+
+    let openssl_cnf = fs::read_to_string("/etc/ssl/openssl.cnf")?;
+    let dns_san = dns.iter().map(|_d| format!("DNS:{}", _d)).collect::<Vec<String>>().join(",");
+    let dns_san = format!("{}\n[SAN]\nsubjectAltName={}", openssl_cnf, dns_san);
+    let _ = write_file(&tmp, &dns_san.as_bytes())?;
+
+    let a =
+        ["req", "-new", "-key", &domain_key_path, "-subj", "/", "-reqexts", "SAN", "-config", &tmp, "-out", &domain_csr_path];
+    let out = Command::new("openssl").args(a).output()?;
+    trace!("{:?}", out);
+
+    let b = ["req", "-in", &domain_csr_path, "-outform", "DER"];
+    let out = Command::new("openssl").args(b).output()?;
+    let csr = base64(&out.stdout);
+    trace!("{}", csr);
+
+    Ok((csr, domain_key_path))
+}
+
+// 分隔，第一个是domain.crt
+pub fn split_cert_chained(crt_str: &str) -> Result<String, AcmeError> {
+    if crt_str.matches(CERT_BEGIN).count() == 1 {
+        warn!("Not full chained crt");
+        return Ok(crt_str.to_string());
+    }
+    Ok(regx1(crt_str, CERT_REGEX)?)
+}
+
+pub fn print_key_by_cmd_openssl(account_key_path: &str, is_ecc: bool) -> Result<String, AcmeError> {
+    let alg = if is_ecc { "ec" } else { "rsa" };
+    let out = Command::new("openssl")
+        .args([alg, "-in", account_key_path, "-noout", "-text"])
+        .output()?;
+    let out = String::from_utf8(out.stdout).unwrap();
+    trace!("out: \n{}", &out);
+
+    Ok(out)
+}
+
+pub fn sign_by_cmd_openssl(account_key_path: &str, plain: &str, is_ecc: bool, alg_len: &str) -> Result<String, AcmeError> {
+    let sha = format!("-sha{}", &alg_len);
+    let mut child = Command::new("openssl")
+        .args(&["dgst", &sha, "-sign", &account_key_path])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped()) // 捕获输出
+        .spawn()?;
+
+    {
+        let stdin = child.stdin.as_mut().expect("Failed to open stdin");
+        stdin.write_all(&plain.as_bytes())?
+    }
+
+    trace!(
+        "echo \"{}\" | openssl dgst -sha256 -sign {} {} | openssl base64",
+        &plain,
+        &account_key_path,
+        if is_ecc { "| openssl asn1parse -inform DER" } else { "" }
+    );
+
+    let out = child.wait_with_output()?;
+    if is_ecc {
+        let mut child = Command::new("openssl")
+            .args(&["asn1parse", "-inform", "DER"])
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped()) // 捕获输出
+            .spawn()?;
+
+        {
+            let stdin = child.stdin.as_mut().expect("Failed to open stdin");
+            stdin.write_all(&out.stdout)?
+        }
+        let out = child.wait_with_output()?;
+        let out = String::from_utf8(out.stdout).unwrap();
+        trace!("sign ecc out:\n{}", &out);
+
+        Ok(base64_hex(&_asn1_parse(&out)?))
+    } else {
+        let out = base64(&out.stdout);
+        trace!("sign rsa out: {}", &out);
+        Ok(out)
+    }
+}
+
+pub fn issuer_cmd_openssl(cert: &str) -> Result<String, AcmeError> {
+    let intermediate_cert = regx2(&cert, CERT_REGEX)?;
+    //debug!("Show intermediate cert:\n{}", &intermediate_cert);
+    x509_one_cmd_openssl(&intermediate_cert)
+}
+// openssl crl2pkcs7 -nocrl -certfile ca.crt | openssl pkcs7 -print_certs -text -noout
+// openssl crl2pkcs7 -nocrl -certfile ca.crt > ca.pk7
+// openssl pkcs7 -print_certs -in ca.pk7 -text -noout
+pub fn x509_one_cmd_openssl(cert: &str) -> Result<String, AcmeError> {
+    let mut child = Command::new("openssl")
+        .args(&["x509", "-text", "-noout"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped()) // 捕获输出
+        .spawn()?;
+
+    {
+        let stdin = child.stdin.as_mut().expect("Failed to open stdin");
+        stdin.write_all(&cert.as_bytes())?
+    }
+    let out = child.wait_with_output()?;
+    let out = String::from_utf8(out.stdout).unwrap();
+    debug!("Show X509 first cert:\n{}", &out);
+
+    let issuer = regx1(&out, ISSUER_REGEX)?.to_uppercase();
+    debug!("Issuer:{}", &issuer);
+
+    Ok(issuer)
+}
+
+/*
+    0:d=0  hl=2 l=  70 cons: SEQUENCE
+    2:d=1  hl=2 l=  33 prim: INTEGER           :9A610C19E73BE8EC7E9BDD8E87B8263BFEA000AA37CFB30A893CD8BC2CA0A3F7
+   37:d=1  hl=2 l=  33 prim: INTEGER           :CE3F47012A7EB61095338B38D95B18E7CDB2EEFFA2BA26E83B226B9C58370A21
+*/
+fn _asn1_parse(text: &str) -> Result<String, AcmeError> {
+    let re = r"prim: INTEGER\s*:\s*(\w+)";
+    //let re = Regex::new(r"prim: INTEGER\s*:\s*(\w+)").unwrap();
+    // let ec_r = re.captures(text).unwrap().get(1).map_or("", |m| m.as_str());
+    // let ec_s = re
+    //     .captures_iter(text)
+    //     .nth(1)
+    //     .and_then(|cap| cap.get(1))
+    //     .map_or("", |m| m.as_str());
+    let (ec_r, ec_s) = (regx1(text, re)?, regx2(text, re)?);
+
+    trace!("ec_r: {}\nec_s: {}", ec_r, ec_s);
+
+    Ok(format!("{}{}", ec_r, ec_s))
+}
+
+pub fn regx2(text: &str, reg: &str) -> Result<String, AcmeError> {
+    let non_greedy_re = Regex::new(reg)?;
+    let p = non_greedy_re
+        .captures_iter(text)
+        .nth(1)
+        .and_then(|cap| cap.get(1))
+        .map_or("", |m| m.as_str());
+    Ok(p.to_string())
+}
+pub fn regx1(text: &str, reg: &str) -> Result<String, AcmeError> {
+    regx(text, reg, false)
+}
+pub fn regx(text: &str, reg: &str, need_rep: bool) -> Result<String, AcmeError> {
+    let non_greedy_re = Regex::new(reg)?;
+    if let Some(cap) = non_greedy_re.captures(&text) {
+        let p = cap.get(1).map_or("", |m| m.as_str());
+        if need_rep {
+            return Ok(p.replace(":", "").replace("\n", "").replace(" ", ""));
+        }
+        Ok(p.to_string())
+    } else {
+        return AcmeError::tip(TIP_REGEX_FAILED);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,514 @@
+mod acme;
+mod crypt;
+
+use acme::acme_issue;
+use chrono::{Duration as ChronoDuration, Local, NaiveDateTime, TimeZone};
+use log::{debug, error, info, Level, LevelFilter, Log, Record};
+use serde::Deserialize;
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    io::Write,
+    path::Path,
+    time::{Duration, Instant, SystemTime, UNIX_EPOCH},
+};
+use tokio::time::sleep_until;
+
+const URL_LE: &str = "https://acme-v02.api.letsencrypt.org/directory"; //test https://acme-staging-v02.api.letsencrypt.org/directory
+const URL_LE_INTERMEDIATE_RSA: &str = "https://letsencrypt.org/certs/2024/r10.pem"; // https://letsencrypt.org/zh-cn/certificates/
+const URL_LE_INTERMEDIATE_ECC: &str = "https://letsencrypt.org/certs/2024/e5.pem";
+
+const URL_ZERO: &str = "https://acme.zerossl.com/v2/DV90";
+const URL_ZERO_EAB: &str = "https://api.zerossl.com/acme/eab-credentials-email";
+//const URL_ZERO_INTERMEDIATE_RSA: &str = "https://crt.sh/?d=1282303295"; // https://help.zerossl.com/hc/en-us/articles/360060198034-Legacy-Client-Compatibility-Cross-Signed-Root-Certificates
+//const URL_ZERO_INTERMEDIATE_ECC: &str = "https://crt.sh/?d=1282303296";
+
+//注意大陆境内，该链接无法访问
+const URL_GOOGLE_TRUST: &str = "https://dv.acme-v02.api.pki.goog/directory";
+//const URL_GTS_INTERMEDIATE_RSA: &str = "https://i.pki.goog/r1.pem"; // https://pki.goog/repository/
+//const URL_GTS_INTERMEDIATE_ECC: &str = "https://i.pki.goog/r4.pem"; // https://i.pki.goog/we1x.pem
+
+const DIR_CA_LE: &str = "/letsencrypt/v02";
+const DIR_CA_ZERO: &str = "/zerossl/v2/DV90";
+const DIR_CA_GOOGLE_TRUST: &str = "/goog/v02";
+
+const DIR_CHALLENGE: &str = "/.well-known/acme-challenge/";
+const DIR_ACME: &str = "/.acme";
+const DIR_BACKUP: &str = "/backup";
+const PATH_CACHE_KID: &str = "/.cache.kid";
+const PATH_ACCOUNT_KEY: &str = "/account.key";
+
+const DOMAIN_KEY: &str = "domain.key";
+const DOMAIN_CRT: &str = "domain.crt";
+const CHAINED_CRT: &str = "chained.crt";
+const DOMAIN_SSL3: [&str; 3] = [DOMAIN_KEY, DOMAIN_CRT, CHAINED_CRT];
+
+const CERT_BEGIN: &str = "-----BEGIN CERTIFICATE-----";
+const CERT_REGEX: &str = r"(-----BEGIN CERTIFICATE-----[\s\S]*?-----END CERTIFICATE-----)";
+const ISSUER_REGEX: &str = r"Issuer:.*CN\s?=\s?(.*)";
+
+const PUB_ECC_REGEX: &str = r"pub:\n\s+([0-9a-fA-F:]+(?:\n\s+[0-9a-fA-F:]+)*)";
+const PUB_RSA_REGEX: &str = r"modulus:[\s]+?00:([a-f0-9\:\s]+?)\npublicExponent: ([0-9]+)";
+
+const CA_DEFAULT_LE: &str = "le";
+const ACCOUNT_ALG_DEFAULT_EC2: &str = "EC2"; //acme接口签名等使用ecc256签名
+const DOMAIN_ALG_DEFAULT_EC3: &str = "EC3"; //域名默认使用ecc384算法，可通过alg参数自定义
+const ALG_HMAC_256: &str = "HS256"; //eab使用该算法签名
+
+const CRON_DEFAULT_DAYS: i64 = 88; //每隔88天
+const CRON_DEFAULT_HOUR: u32 = 0; //下一次0点执行
+
+const CACHE_EXPIRE_DAY: u64 = 7; //7天过期
+const CACHE_EXPIRE_SEC: u64 = 60 * 60 * 24 * CACHE_EXPIRE_DAY; //时间戳，7天过期
+const MAX_TRY: u8 = 8; //重试最大次数
+const SLEEP_DURATION_SEC_2: Duration = Duration::from_secs(2); //2s
+const SLEEP_DURATION_SEC_5: Duration = Duration::from_secs(5); //5s
+const LOG_LEVEL_DEAULT: LevelFilter = LevelFilter::Debug;
+
+const FMT_LOCAL_TIME: &str = "%m/%d %H:%M:%S%.3f"; // 07/08 09:53:37.520
+
+const TIP_MISSING_DNS: &str = "Missing parameter 'dns'";
+const TIP_MISSING_DIR: &str = "Missing parameter 'dir'";
+const TIP_INVALID_DNS: &str = "Wildcard domain names like *.a.com are not supported. Exactly, such as: dns=ai8.rs,www.ai8.rs";
+const TIP_REGEX_FAILED: &str = "Match Regex Fialed.";
+
+pub async fn simple_cron(cfg: &AcmeCfg) -> Result<(), AcmeError> {
+    // quartz
+    // let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(90 * 24 * 60 * 60)); //90天
+    // loop {
+    //     interval.tick().await;
+    //     let _ = acme_issue3(&cfg).await;
+    // }
+    _simple_cron(cfg, CRON_DEFAULT_DAYS, CRON_DEFAULT_HOUR).await
+}
+
+pub struct AcmeLogger;
+impl Log for AcmeLogger {
+    fn enabled(&self, metadata: &log::Metadata) -> bool {
+        metadata.level() <= Level::Trace // 记录 Trace 及以上级别的日志
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            let f = record
+                .file()
+                .map(|f| Path::new(f).file_name().and_then(|s| s.to_str()).unwrap_or(""))
+                .unwrap_or("unknown file");
+            if f == "connect.rs" {
+                return;
+            }
+            let now_local = Local::now().format(FMT_LOCAL_TIME);
+            let msg = format!(
+                "{} {:5} [{}:{}] - {}",
+                now_local,
+                record.level(),
+                f,
+                record.line().unwrap_or(0),
+                record.args()
+            );
+            println!("{}", msg);
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+#[derive(Debug)]
+pub struct AcmeCfg {
+    dns: Vec<String>,
+    email: Option<String>,
+    acme_root: String,   //根目录，如 /www/ai8.rs
+    acme_ca_dir: String, //acme运行的ca相关，如一些缓存，如 /www/ai8.rs/.acme/letsencrypt/v02
+    ssl_dir: String,     //domin.key,chained.pem等文件复制到指定的证书目录下, 默认/www/ai8.rs/.acme/ssl
+    ca: AcmeCa,
+    preferred_chain: Option<String>,
+    domain_alg: Alg,
+    pub log_level: LevelFilter,
+    eab: Eab,
+}
+
+impl AcmeCfg {
+    pub fn new(args: Vec<String>) -> Result<Self, AcmeError> {
+        let mut map: HashMap<&str, &str> = HashMap::new();
+        for arg in args.iter() {
+            //let (k, v) = arg.split_once('=').ok_or_else(|| AcmeError::Tip("参数格式错误".to_string()))?;
+            //map.insert(k, v);
+            if let Some((k, v)) = arg.split_once('=') {
+                map.insert(k, v);
+            }
+        }
+
+        let dns = map.get("dns").ok_or_else(|| AcmeError::Tip(TIP_MISSING_DNS.to_string()))?;
+
+        if dns.contains("*") {
+            return AcmeError::tip(TIP_INVALID_DNS);
+        }
+        let dns = dns.split(",").map(|s| s.to_string()).collect();
+
+        let acme_root = map
+            .get("dir")
+            .ok_or_else(|| AcmeError::Tip(TIP_MISSING_DIR.to_string()))?
+            .to_string();
+
+        if !Path::new(&acme_root).is_dir() {
+            return AcmeError::tip(&format!("The directory does not exist: {}", acme_root));
+        }
+
+        let email = map.get("email").map(|s| s.to_string());
+        let ca = AcmeCa::new(map.get("ca").unwrap_or(&CA_DEFAULT_LE));
+        let domain_alg = Alg::new(map.get("alg").unwrap_or(&DOMAIN_ALG_DEFAULT_EC3));
+
+        let acme_dir = format!("{0}{1}", acme_root, DIR_ACME);
+        let acme_ca_dir = format!("{0}{1}", acme_dir, ca.ca_dir());
+
+        let ssl_dir = map.get("ssl_dir").map(|s| s.to_string()).unwrap_or(acme_dir);
+        let _path = Path::new(&acme_ca_dir);
+        if !_path.exists() {
+            debug!("Create path: {:?}", _path);
+            fs::create_dir_all(_path)?; // 递归创建目录
+        }
+
+        let preferred_chain = map
+            .get("preferred_chain")
+            .map(|s| s.to_string())
+            .or(ca.preferred_chain(domain_alg.is_ecc()));
+
+        let log_level = match map.get("log") {
+            Some(level) => match level.to_lowercase().as_str() {
+                "debug" => LevelFilter::Debug,
+                "trace" => LevelFilter::Trace,
+                _ => LOG_LEVEL_DEAULT,
+            },
+            _ => LOG_LEVEL_DEAULT,
+        };
+
+        let eab_kid = map.get("eab_kid").map(|s| s.to_string());
+        let eab_hmac_key = map.get("eab_key").map(|s| s.to_string());
+        let eab = Eab::new(eab_kid, eab_hmac_key);
+
+        Ok(AcmeCfg {
+            dns,
+            email,
+            acme_root,
+            acme_ca_dir,
+            ssl_dir,
+            ca,
+            preferred_chain,
+            domain_alg,
+            log_level,
+            eab,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub enum AcmeError {
+    ReqwestError(reqwest::Error),
+    IoError(std::io::Error),
+    TimeError(std::time::SystemTimeError),
+    SerdeJsonError(serde_json::Error),
+    RegexError(regex::Error),
+    Tip(String), // 自定义错误
+}
+
+impl From<reqwest::Error> for AcmeError {
+    fn from(error: reqwest::Error) -> Self {
+        AcmeError::ReqwestError(error)
+    }
+}
+impl From<std::io::Error> for AcmeError {
+    fn from(error: std::io::Error) -> Self {
+        AcmeError::IoError(error)
+    }
+}
+
+impl From<std::time::SystemTimeError> for AcmeError {
+    fn from(error: std::time::SystemTimeError) -> Self {
+        AcmeError::TimeError(error)
+    }
+}
+impl From<serde_json::Error> for AcmeError {
+    fn from(error: serde_json::Error) -> Self {
+        AcmeError::SerdeJsonError(error)
+    }
+}
+impl From<regex::Error> for AcmeError {
+    fn from(error: regex::Error) -> Self {
+        AcmeError::RegexError(error)
+    }
+}
+
+impl AcmeError {
+    pub fn to_string(&self) -> String {
+        match &self {
+            AcmeError::ReqwestError(_e) => _e.to_string(),
+            AcmeError::IoError(_e) => _e.to_string(),
+            AcmeError::TimeError(_e) => _e.to_string(),
+            AcmeError::SerdeJsonError(_e) => _e.to_string(),
+            AcmeError::RegexError(_e) => _e.to_string(),
+            AcmeError::Tip(_e) => _e.to_string(),
+        }
+    }
+
+    fn tip<T>(s: &str) -> Result<T, AcmeError> {
+        Err(AcmeError::Tip(s.to_string()))
+    }
+}
+
+async fn _simple_cron(cfg: &AcmeCfg, days: i64, hour: u32) -> Result<(), AcmeError> {
+    let mut next_execution = Local::now().naive_local(); //_next_execution(Utc::now().naive_utc(), days, hour);
+    loop {
+        let future = sleep_until(_tokio_instant(next_execution)?);
+        tokio::pin!(future);
+        future.await;
+
+        let _ = _acme_issue3(&cfg).await;
+
+        let next_date = next_execution + ChronoDuration::days(days);
+        next_execution = next_date.date().and_hms_opt(hour, 0, 0).unwrap();
+    }
+}
+fn _tokio_instant(next_execution: NaiveDateTime) -> Result<tokio::time::Instant, AcmeError> {
+    let datetime = Local.from_local_datetime(&next_execution).unwrap();
+    // 将 DateTime<Utc> 转换为 Instant
+    let tt = datetime.timestamp();
+    let st = SystemTime::UNIX_EPOCH + Duration::from_secs(tt as u64);
+    let it = Instant::now();
+    let instant = st.duration_since(SystemTime::now()).ok().map_or(it, |d| it + d);
+
+    info!("Next execution: {:?}, {}, {:?}", datetime, tt, instant);
+    Ok(tokio::time::Instant::from_std(instant))
+}
+
+// fn _next_execution(now: NaiveDateTime, days: i64, hour: u32) -> NaiveDateTime {
+//     let mut execution_date = now.date().and_hms_opt(hour, 0, 0).unwrap(); // 设置为当前日期的8点
+//     if execution_date < now {
+//         execution_date = execution_date + ChronoDuration::days(days); // 如果已经过了8点，就设置为下一个90天
+//     }
+//     while execution_date.signed_duration_since(now).num_days() % days != 0 {
+//         execution_date = execution_date + ChronoDuration::days(1);
+//     }
+//     execution_date
+// }
+
+async fn _acme_issue3(cfg: &AcmeCfg) -> () {
+    // retry 2
+    for i in 0..3 {
+        debug!("Loop {}/3, acme issue", i + 1);
+        match acme_issue(&cfg).await {
+            Err(_e) => error!("{}", _e.to_string()),
+            Ok(paths) => {
+                info!(
+        		"Successfully.\nFor Nginx configuration:\n\tssl_certificate {1};\n\tssl_certificate_key {2};
+        		\nFor Apache configuration:\n\tSSLEngine on\n\tSSLCertificateFile {0}\n\tSSLCertificateKeyFile {1}\n\tSSLCertificateChainFile {2}",
+        		&paths[0], &paths[1], &paths[2]
+        	);
+                return;
+            }
+        }
+        std::thread::sleep(SLEEP_DURATION_SEC_5);
+    }
+}
+
+fn read_cache(_cache_path: &str) -> Option<String> {
+    if let Err(e) = _cache_expire_then_rm(&_cache_path) {
+        debug!("Ignore expire cache error: {:?}", e);
+        return None;
+    }
+
+    match fs::read_to_string(&_cache_path) {
+        Ok(s) => {
+            debug!("Hit cache: {}", _cache_path);
+            Some(s)
+        }
+        Err(_) => None,
+    }
+}
+
+fn _cache_expire_then_rm(file_path: &str) -> Result<(), AcmeError> {
+    let path = Path::new(file_path);
+    if !path.exists() {
+        return Ok(());
+    }
+    let modified_time = fs::metadata(path)?.modified()?.duration_since(UNIX_EPOCH)?.as_secs();
+    let now = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
+    if modified_time + CACHE_EXPIRE_SEC < now {
+        debug!(
+            "Cache expired. File modified > {} days, then rm: {}",
+            CACHE_EXPIRE_DAY, &file_path
+        );
+        fs::remove_file(path)?;
+    }
+    Ok(())
+}
+
+// 覆盖写入
+fn write_file(file_path: &str, s: &[u8]) -> Result<(), AcmeError> {
+    let _ = File::create(&file_path)
+        .map_err(|_e| AcmeError::Tip(format!("Create file failed: {}. {}", file_path, _e.to_string())))?
+        .write(s)
+        .map_err(|_| AcmeError::Tip(format!("Write failed: {}", file_path)));
+    debug!("Write to {}", file_path);
+    Ok(())
+}
+
+trait CA {
+    fn ca_dir(&self) -> &'static str;
+    fn directory_url(&self) -> &'static str;
+}
+struct LetsEncrypt;
+struct ZeroSSL;
+struct GoogleTrustServices;
+
+impl CA for LetsEncrypt {
+    fn ca_dir(&self) -> &'static str {
+        DIR_CA_LE
+    }
+    fn directory_url(&self) -> &'static str {
+        URL_LE
+    }
+}
+
+impl CA for ZeroSSL {
+    fn ca_dir(&self) -> &'static str {
+        DIR_CA_ZERO
+    }
+    fn directory_url(&self) -> &'static str {
+        URL_ZERO
+    }
+}
+
+impl CA for GoogleTrustServices {
+    fn ca_dir(&self) -> &'static str {
+        DIR_CA_GOOGLE_TRUST
+    }
+    fn directory_url(&self) -> &'static str {
+        URL_GOOGLE_TRUST
+    }
+}
+
+enum AcmeCa {
+    LetsEncrypt(Box<dyn CA>),
+    ZeroSSL(Box<dyn CA>),
+    GoogleTrustServices(Box<dyn CA>),
+}
+
+// 只是简单实现 Debug trait，并不会真实打印。编译器不会再报错
+impl std::fmt::Debug for AcmeCa {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AcmeCa::LetsEncrypt(_) => write!(f, "AcmeCa::LetsEncrypt"),
+            AcmeCa::ZeroSSL(_) => write!(f, "AcmeCa::ZeroSSL"),
+            AcmeCa::GoogleTrustServices(_) => write!(f, "AcmeCa::GoogleTrustServices"),
+        }
+    }
+}
+
+impl AcmeCa {
+    fn new(ca_type: &str) -> Self {
+        match ca_type {
+            CA_DEFAULT_LE => AcmeCa::LetsEncrypt(Box::new(LetsEncrypt)),
+            "z" | "zero" => AcmeCa::ZeroSSL(Box::new(ZeroSSL)),
+            "g" | "gts" => AcmeCa::GoogleTrustServices(Box::new(GoogleTrustServices)),
+            _ => AcmeCa::LetsEncrypt(Box::new(LetsEncrypt)),
+        }
+    }
+    fn ca_dir(&self) -> String {
+        self.ca().ca_dir().to_string()
+    }
+    fn directory_url(&self) -> String {
+        self.ca().directory_url().to_string()
+    }
+    fn eab_url(&self) -> Option<String> {
+        match self {
+            AcmeCa::ZeroSSL(_) => Some(URL_ZERO_EAB.to_string()),
+            _ => None,
+        }
+    }
+    fn preferred_chain(&self, is_ecc: bool) -> Option<String> {
+        match self {
+            AcmeCa::LetsEncrypt(_) => Some(format!("ISRG ROOT X{}", if is_ecc { "2" } else { "1" })),
+            _ => None,
+        }
+    }
+    fn intermediate_url(&self, is_ecc: bool) -> Option<String> {
+        match self {
+            AcmeCa::LetsEncrypt(_) => Some(
+                if is_ecc {
+                    URL_LE_INTERMEDIATE_ECC
+                } else {
+                    URL_LE_INTERMEDIATE_RSA
+                }
+                .to_string(),
+            ),
+            _ => None,
+        }
+    }
+    fn ca(&self) -> &dyn CA {
+        match self {
+            AcmeCa::LetsEncrypt(ca) => ca.as_ref(),
+            AcmeCa::ZeroSSL(ca) => ca.as_ref(),
+            AcmeCa::GoogleTrustServices(ca) => ca.as_ref(),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Alg {
+    RSA2048,
+    RSA4096,
+    ECC256,
+    ECC384,
+}
+impl Alg {
+    fn new(alg: &str) -> Self {
+        match alg.to_uppercase().as_str() {
+            DOMAIN_ALG_DEFAULT_EC3 | "ECC3" => Alg::ECC384,
+            "EC2" | "ECC2" => Alg::ECC256,
+            "RSA4" => Alg::RSA4096,
+            "RSA2" => Alg::RSA2048,
+            _ => Alg::ECC384, //default
+        }
+    }
+
+    fn is_ecc(&self) -> bool {
+        matches!(self, Alg::ECC256 | Alg::ECC384)
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct Eab {
+    success: bool,
+    error: Option<EabError>,
+    eab_kid: Option<String>,
+    eab_hmac_key: Option<String>,
+}
+impl Eab {
+    fn new(eab_kid: Option<String>, eab_hmac_key: Option<String>) -> Self {
+        Eab {
+            success: eab_kid.is_some() & eab_hmac_key.is_some(),
+            eab_kid,
+            eab_hmac_key,
+            error: None,
+        }
+    }
+    fn _clone(eab: &Eab) -> Option<Self> {
+        Some(Self::new(eab.eab_kid.clone(), eab.eab_hmac_key.clone()))
+    }
+
+    fn _kid(&self) -> String {
+        if self.success {
+            format!(".{}", self.eab_kid.clone().unwrap())
+        } else {
+            "".to_string()
+        }
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct EabError {
+    code: u32,
+    #[serde(rename(deserialize = "type"))]
+    _type: Option<String>,
+    info: Option<String>,
+}
